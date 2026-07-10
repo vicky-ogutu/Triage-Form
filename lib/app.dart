71ecb28a0f1381e3ae79892cba401f2935dc0d2e
vicky-ogutu <vikogutu77@gmail.com>
2026-07-10 12:12:44 +0300

    initialize Hive

---
+++ lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'ui/triage_form.dart';
+import 'providers/providers.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'EMS Triage Form',
+        theme: ThemeData.dark(),
+        home: const TriageForm(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
